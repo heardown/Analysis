78f29bc2bdb45c30095d0447559b2d66be660597
winds <heardown@163.com>
2020-07-17 18:09:29 +0800

    sync

--- track/src/main/java/com/sayweee/track/model/TrackEvent.java
+++ track/src/main/java/com/sayweee/track/model/TrackEvent.java
@@ -4,7 +4,7 @@ package com.sayweee.track.model;
  * Author:  winds
  * Email:   heardown@163.com
  * Date:    20120/7/16.
- * Desc:
+ * Desc:    标准事件
  */
 public class TrackEvent {
 }
--- track/src/main/java/com/sayweee/track/model/TrackEventParameter.java
+++ track/src/main/java/com/sayweee/track/model/TrackEventParameter.java
@@ -4,7 +4,7 @@ package com.sayweee.track.model;
  * Author:  winds
  * Email:   heardown@163.com
  * Date:    20120/7/16.
- * Desc:
+ * Desc:    标准事件参数
  */
 public class TrackEventParameter {
 }
